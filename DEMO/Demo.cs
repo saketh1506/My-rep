9c4983b7f22d9eed7545b8d3abacb8979a744da0
KANDURI SAKETH SAI NIGAM <61182094+saketh1506@users.noreply.github.com>
2021-08-18 16:48:32 +0530

    Add files via upload

---
+++ DEMO/Demo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DEMO/Demo1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyFirstConsoleApp
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+           double b = 1.2;
+            char c = 's';
+            bool d = true;
+            string e = "saketh sai nigam";
+            Console.WriteLine("Examples of each Dataypes of c# {0},{1},{2},{3},{4}", a,b,c,d,e);
+        }
+    }
+}
